e5b0dbbf1282f3b9894ee4d2a3b513f7c7a36f87
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2022-10-04 22:55:02 +0200

    Add files via upload
    initial commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kudzayi  Bamhare on 10/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
